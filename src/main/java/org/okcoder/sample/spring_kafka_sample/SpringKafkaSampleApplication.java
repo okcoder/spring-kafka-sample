e02e4c598e2fe0d65bd83f26b377b4097532b529
okcoder <okcoder@live.com>
2023-12-01 21:49:55 +0900

    create project

---
+++ src/main/java/org/okcoder/sample/spring_kafka_sample/SpringKafkaSampleApplication.java
@@ -0,0 +1,13 @@
+package org.okcoder.sample.spring_kafka_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/okcoder/sample/spring_kafka_sample/SpringKafkaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.okcoder.sample.spring_kafka_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
